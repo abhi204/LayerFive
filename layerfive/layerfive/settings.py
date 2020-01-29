3c2f5eb50a2984a40b62a0470bf6f2bbb6b95aab
Abhi <abhi.chaurasia204@gmail.com>
2020-01-29 16:23:25 +0530

    Enable cors

--- layerfive/layerfive/settings.py
+++ layerfive/layerfive/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'user.apps.UserConfig',
 ]
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
